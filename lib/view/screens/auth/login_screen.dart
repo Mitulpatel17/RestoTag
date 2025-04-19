f1486d22db7178efd01b067e060b5aca7532a5b8
VasuGajjar <vasugajjar1@gmail.com>
2025-04-19 14:47:59 +0530

    ui changes

--- lib/view/screens/auth/login_screen.dart
+++ lib/view/screens/auth/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:restotag_customer_app/view/base/resizable_scrollview.dart';
 import 'package:restotag_customer_app/view/screens/auth/otp_screen.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -10,59 +9,61 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      body: ResizableScrollView(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        padding: EdgeInsets.all(36),
-        children: [
-          Spacer(),
-          Hero(
-            tag: 'title',
-            child: Text(
-              'Sign In',
-              style: Theme.of(context).textTheme.headlineMedium,
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Hero(
-            tag: 'subtitle',
-            child: Text(
-              'Verify your phone number',
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Spacer(),
-          Hero(
-            tag: 'number',
-            child: TextField(
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                labelText: 'Mobile Number',
-                icon: Icon(Icons.phone_outlined),
-              ),
-              keyboardType: TextInputType.phone,
-            ),
-          ),
-          Spacer(),
-          Hero(
-            tag: 'main_btn',
-            child: FilledButton.icon(
-              onPressed: () => Get.off(const OtpScreen()),
-              label: Text('SEND OTP'),
-              icon: Icon(Icons.arrow_forward),
-              style: ButtonStyle(
-                padding: WidgetStatePropertyAll(EdgeInsets.all(18)),
+      body: Padding(
+        padding: const EdgeInsets.all(36),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Spacer(),
+            Hero(
+              tag: 'title',
+              child: Text(
+                'Sign In',
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
               ),
             ),
-          ),
-          SizedBox(height: 16),
-          Hero(
-            tag: 'scnd_btn',
-            child: TextButton(onPressed: () {}, child: Text('CREATE ACCOUNT')),
-          ),
-          Spacer(),
-        ],
+            Hero(
+              tag: 'subtitle',
+              child: Text(
+                'Verify your phone number',
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Spacer(),
+            Hero(
+              tag: 'number',
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  labelText: 'Mobile Number',
+                  icon: Icon(Icons.phone_outlined),
+                ),
+                keyboardType: TextInputType.phone,
+              ),
+            ),
+            Spacer(),
+            Hero(
+              tag: 'main_btn',
+              child: FilledButton.icon(
+                onPressed: () => Get.off(const OtpScreen()),
+                label: Text('SEND OTP'),
+                icon: Icon(Icons.arrow_forward),
+                style: ButtonStyle(
+                  padding: WidgetStatePropertyAll(EdgeInsets.all(18)),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Hero(
+              tag: 'scnd_btn',
+              child: TextButton(onPressed: () {}, child: Text('CREATE ACCOUNT')),
+            ),
+            Spacer(),
+          ],
+        ),
       ),
     );
   }
--- lib/view/screens/auth/otp_screen.dart
+++ lib/view/screens/auth/otp_screen.dart
@@ -9,79 +9,81 @@ class OtpScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      body: ResizableScrollView(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        padding: EdgeInsets.all(36),
-        children: [
-          Spacer(),
-          Hero(
-            tag: 'title',
-            child: Text(
-              'Sign In',
-              style: Theme.of(context).textTheme.headlineMedium,
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Hero(
-            tag: 'subtitle',
-            child: Text(
-              'Verify your phone number',
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Spacer(),
-          Hero(
-            tag: 'number',
-            child: TextField(
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                labelText: 'Mobile Number',
-                icon: Icon(Icons.phone_outlined),
-              ),
-              keyboardType: TextInputType.phone,
-              enabled: false,
-            ),
-          ),
-          SizedBox(height: 12),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text('Enter OTP:'),
-              OtpTextField(
-                onCodeChanged: (value) {},
-                onSubmit: (value) {},
-                numberOfFields: 4,
-                keyboardType: TextInputType.number,
-                showFieldAsBox: true,
-                borderColor: Theme.of(context).primaryColor,
-                autoFocus: true,
-                enabledBorderColor: Theme.of(context).focusColor,
-              ),
-            ],
-          ),
-          SizedBox(height: 12),
-          TextButton(onPressed: () {}, child: Text('Resend OTP')),
-          Spacer(),
-          Hero(
-            tag: 'main_btn',
-            child: FilledButton.icon(
-              onPressed: () {},
-              label: Text('SIGN IN'),
-              icon: Icon(Icons.arrow_forward),
-              style: ButtonStyle(
-                padding: WidgetStatePropertyAll(EdgeInsets.all(18)),
+      body: Padding(
+        padding: const EdgeInsets.all(36),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Spacer(),
+            Hero(
+              tag: 'title',
+              child: Text(
+                'Sign In',
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
               ),
             ),
-          ),
-          SizedBox(height: 16),
-          Hero(
-            tag: 'scnd_btn',
-            child: TextButton(onPressed: () {}, child: Text('CREATE ACCOUNT')),
-          ),
-          Spacer(),
-        ],
+            Hero(
+              tag: 'subtitle',
+              child: Text(
+                'Verify your phone number',
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Spacer(),
+            Hero(
+              tag: 'number',
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  labelText: 'Mobile Number',
+                  icon: Icon(Icons.phone_outlined),
+                ),
+                keyboardType: TextInputType.phone,
+                enabled: false,
+              ),
+            ),
+            SizedBox(height: 12),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Enter OTP:'),
+                OtpTextField(
+                  onCodeChanged: (value) {},
+                  onSubmit: (value) {},
+                  numberOfFields: 4,
+                  keyboardType: TextInputType.number,
+                  showFieldAsBox: true,
+                  borderColor: Theme.of(context).primaryColor,
+                  autoFocus: true,
+                  enabledBorderColor: Theme.of(context).focusColor,
+                ),
+              ],
+            ),
+            SizedBox(height: 12),
+            TextButton(onPressed: () {}, child: Text('Resend OTP')),
+            Spacer(),
+            Hero(
+              tag: 'main_btn',
+              child: FilledButton.icon(
+                onPressed: () {},
+                label: Text('SIGN IN'),
+                icon: Icon(Icons.arrow_forward),
+                style: ButtonStyle(
+                  padding: WidgetStatePropertyAll(EdgeInsets.all(18)),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Hero(
+              tag: 'scnd_btn',
+              child: TextButton(onPressed: () {}, child: Text('CREATE ACCOUNT')),
+            ),
+            Spacer(),
+          ],
+        ),
       ),
     );
   }
